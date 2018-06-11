28c9749b31884d25390c816f316dda5cd8f08cf4
srihari <hari.jammula6@gmail.com>
2018-06-11 13:06:38 +0530

    master commit

--- LoggingExample/src/main/java/com/srihari/HelloWorld.java
+++ LoggingExample/src/main/java/com/srihari/HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello srihari");
 		System.out.println("Hello sriharibabu");
-		System.out.println("Hello sriharibabu jammula");
-	}
+		}
 
 }
